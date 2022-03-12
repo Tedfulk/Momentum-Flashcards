afab6ed82751615908ef1a08bc525315f12b3d96
Tedfulk <98295827+Tedfulk@users.noreply.github.com>
2022-03-11 20:11:28 -0500

    models forms database start

--- cards/models.py
+++ cards/models.py
@@ -2,10 +2,30 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
 class User(AbstractUser):
     def __repr__(self):
         return f"<User username={self.username}>"
 
     def __str__(self):
         return self.username
-    
+
+
+class Deck(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
+
+
+class Card(models.Model):
+    word = models.CharField(max_length=500)
+    definition = models.CharField(max_length=500)
+    deck = models.ForeignKey(
+        Deck, related_name="card", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.word
