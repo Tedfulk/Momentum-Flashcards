2765f73e69161b1f58e2534d40eaaf82f97862f6
Brittany Craig <brittanycraig@gmail.com>
2022-03-12 20:22:35 -0500

    changed related name in Deck model per Rebecca's suggestion

--- cards/models.py
+++ cards/models.py
@@ -24,8 +24,10 @@ class Card(models.Model):
     word = models.CharField(max_length=500)
     definition = models.CharField(max_length=500)
     deck = models.ForeignKey(
-        Deck, related_name="card", on_delete=models.CASCADE
+        Deck, related_name="cards", on_delete=models.CASCADE
     )
+    
+# TODO make seperate pull request and change related name to "cards" so we can later do deck.cards
 
     def __str__(self):
         return self.word
